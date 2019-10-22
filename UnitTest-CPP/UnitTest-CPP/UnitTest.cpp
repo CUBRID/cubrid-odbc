c192bfbc18e39b9307fd746081343a49f2ba72a7
Song won-ryong <38000047+swi0110@users.noreply.github.com>
2019-10-22 12:51:10 +0900

    Add to test-code for under version to 9.2.x. (#37)

--- UnitTest-CPP/UnitTest-CPP/UnitTest.cpp
+++ UnitTest-CPP/UnitTest-CPP/UnitTest.cpp
@@ -5,7 +5,7 @@
 #include <windows.h>  
 #include <sql.h>  
 #include <sqlext.h>  
-#include <odbcss.h>  
+#include <odbcss.h>
 
 #define MAXBUFLEN	640
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
@@ -40,8 +40,8 @@ namespace UnitTestCPP
 			/* Allocate a connection handle */
 			SQLAllocHandle(SQL_HANDLE_DBC, env, &dbc);
 
-			retcode = SQLDriverConnect(dbc, NULL, L"DSN=CUBRID_UNI;DB_NAME=demodb1;SERVER=localhost;PORT=33000;USER=dba;PWD=;CHARSET=utf-8;", SQL_NTS, NULL, 0, NULL, SQL_DRIVER_NOPROMPT);
-			//retcode = SQLConnect(dbc, L"CUBRID Driver Unicode", SQL_NTS, L"dba", SQL_NTS, NULL, SQL_NTS);
+			//retcode = SQLDriverConnect(dbc, NULL, L"DSN=CUBRID Driver Unicode;DB_NAME=demodb;SERVER=test-db-server;PORT=33000;USER=dba;PWD=;CHARSET=utf-8;", SQL_NTS, NULL, 0, NULL, SQL_DRIVER_NOPROMPT);
+			retcode = SQLConnect(dbc, L"CUBRID Driver Unicode", SQL_NTS, L"dba", SQL_NTS, NULL, SQL_NTS);
 
 			if (retcode == SQL_ERROR) {
 				SQLGetDiagField(SQL_HANDLE_DBC, dbc, 0, SQL_DIAG_NUMBER, &diag_rec, 0, &plm_pcbErrorMsg);
@@ -103,8 +103,8 @@ namespace UnitTestCPP
 			/* Allocate a connection handle */
 			SQLAllocHandle(SQL_HANDLE_DBC, env, &dbc);
 
-			retcode = SQLDriverConnect(dbc, NULL, L"DSN=CUBRID_UNI;DB_NAME=demodb;SERVER=test-db-server;PORT=55300;USER=dba;PWD=;CHARSET=utf-8;", SQL_NTS, NULL, 0, NULL, SQL_DRIVER_NOPROMPT);
-			//retcode = SQLConnect(dbc, L"CUBRID Driver Unicode", SQL_NTS, L"dba", SQL_NTS, NULL, SQL_NTS);
+			//retcode = SQLDriverConnect(dbc, NULL, L"DSN=CUBRID Driver Unicode;DB_NAME=demodb;SERVER=test-db-server;PORT=33000;USER=dba;PWD=;CHARSET=utf-8;", SQL_NTS, NULL, 0, NULL, SQL_DRIVER_NOPROMPT);
+			retcode = SQLConnect(dbc, L"CUBRID Driver Unicode", SQL_NTS, L"dba", SQL_NTS, NULL, SQL_NTS);
 
 			if (retcode == SQL_ERROR) {
 				SQLGetDiagField(SQL_HANDLE_DBC, dbc, 0, SQL_DIAG_NUMBER, &diag_rec, 0, &plm_pcbErrorMsg);
@@ -143,9 +143,21 @@ namespace UnitTestCPP
 			if (strlen((const char *)query_plan) > 0) {
 				wchar_t expected_window[198] = L"Join graph segments (f indicates final):\r\nseg[0]: [0]\r\nseg[1]: 이름[0] (f)\r\nseg[2]: 나이[0] (f)\r\nJoin graph nodes:\r\nnode[0]: 테이블 테이블(1/1) (sargs 0) (loc 0)\r\nJoin graph terms:\r\nterm[0]: [테이블].[나이] range";
 				wchar_t expected_linux[198] = L"Join graph segments (f indicates final):\nseg[0]: [0]\nseg[1]: 이름[0] (f)\nseg[2]: 나이[0] (f)\nJoin graph nodes:\nnode[0]: 테이블 테이블(1/1) (sargs 0) (loc 0)\nJoin graph terms:\nterm[0]: [테이블].[나이] range";
+				
+				//for windows
 				int c = wcsncmp(query_plan, expected_window, wcslen(expected_window));
 				if (c != 0) {
+					// for linux
 					c = wcsncmp(query_plan, expected_linux, wcslen(expected_linux));
+				}
+				if (c != 0) {
+					wchar_t* wc = (wchar_t*)query_plan;
+					std::wstring ws(wc);
+
+					//for under version to 9.2.x
+					if (((int)ws.find(L"테이블")) > 0 && ((int)ws.find(L"이름")) > 0 && ((int)ws.find(L"나이")) > 0){
+						c = 0;
+					}
 				}
 				Assert::AreEqual(c, 0);
 			}
