5c0af0ba289e9541187350650b2c44ed5c2a5cfd
hwanyseo <hwanyseo@cubrid.com>
2022-04-12 00:01:43 +0900

    license

--- script/get_cci_driver.bat
+++ script/get_cci_driver.bat
@@ -1,5 +1,22 @@
 @echo off
 
+REM
+REM  Copyright 2008 Search Solution Corporation
+REM  Copyright 2016 CUBRID Corporation
+REM 
+REM   Licensed under the Apache License, Version 2.0 (the "License");
+REM   you may not use this file except in compliance with the License.
+REM   You may obtain a copy of the License at
+REM 
+REM       http://www.apache.org/licenses/LICENSE-2.0
+REM 
+REM   Unless required by applicable law or agreed to in writing, software
+REM   distributed under the License is distributed on an "AS IS" BASIS,
+REM   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+REM   See the License for the specific language governing permissions and
+REM   limitations under the License.
+REM 
+
 set SCRIPT_DIR=%~dp0
 set CCI_WIN_DIR=%SCRIPT_DIR%\cubrid-cci\win\cas_cci
 set CCI_SRC_DIRS=%SCRIPT_DIR%\cubrid-cci\src\cci
