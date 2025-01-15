8b4a3256f9aef678e0ce16442985c3e2bf1f8001
Kang-dot <jgbu97@gmail.com>
2025-01-15 17:27:04 +0900

    update test project
    - change test tool to "vstest"
    - edit test project for x64 test

--- test.bat
+++ test.bat
@@ -1,5 +1,7 @@
+set VSTestTool=%VS2017COMNTOOLS%..\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe
 call "%VS2017COMNTOOLS%VsDevCmd.bat"
 cd UnitTest
-devenv UnitTest.sln /project UnitTest\UnitTest.csproj /rebuild "Release|x64" 
+devenv UnitTest_14.sln /Upgrade
+devenv UnitTest_14.sln /Project UnitTest\UnitTest.csproj /Rebuild "Release|x64" 
 cd UnitTest\bin\x64\Release
-MSTest/testcontainer:UnitTest.dll
+call "%VSTestTool%" UnitTest.dll --platform:x64
