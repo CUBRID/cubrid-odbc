62fc4f47b754dd46efac17df87ab4db1ee35395c
Kang-dot <jgbu97@gmail.com>
2025-01-20 16:31:46 +0900

    add UnitTest-CPP
    - add CPP unit test in test.bat

--- test.bat
+++ test.bat
@@ -1,3 +1,4 @@
+set BATCH_HOME=%~dp0
 set VSTestTool=%VS2017COMNTOOLS%..\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe
 call "%VS2017COMNTOOLS%VsDevCmd.bat"
 cd UnitTest
@@ -5,3 +6,9 @@ devenv UnitTest_14.sln /Upgrade
 devenv UnitTest_14.sln /Project UnitTest\UnitTest.csproj /Rebuild "Release|x64" 
 cd UnitTest\bin\x64\Release
 call "%VSTestTool%" UnitTest.dll --platform:x64
+
+cd %BATCH_HOME%\UnitTest-CPP
+devenv UnitTest_14.sln /Upgrade
+devenv UnitTest-CPP.sln /Project UnitTest-CPP\UnitTest-CPP.vcxproj /Rebuild "Release|x64" 
+cd x64\Release
+call "%VSTestTool%" UnitTest-CPP.dll --platform:x64
