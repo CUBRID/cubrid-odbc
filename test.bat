0ba5ffb6bcb45a8719c6a2d07ff7cd2929741038
KangBu Jeong <54855268+Kang-dot@users.noreply.github.com>
2025-01-23 14:50:21 +0900

    [APIS-1007] edit test project (#78)
    - change test tool to "vstest"
    - edit test project for x64 test
    
    - add CPP unit test in test.bat
    - edit unit test cpp project for x64 test
    
    - add file output function in test batch file

--- test.bat
+++ test.bat
@@ -1,5 +1,23 @@
+set BATCH_HOME=%~dp0
+set VSTESTTOOL=%VS2017COMNTOOLS%..\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe
 call "%VS2017COMNTOOLS%VsDevCmd.bat"
 cd UnitTest
-devenv UnitTest.sln /project UnitTest\UnitTest.csproj /rebuild "Release|x64" 
+devenv UnitTest_14.sln /Project UnitTest\UnitTest.csproj /Rebuild "Release|x64" 
 cd UnitTest\bin\x64\Release
-MSTest/testcontainer:UnitTest.dll
+ 
+if "%1"=="-p" (
+  powershell -Command "& '%VSTESTTOOL%' UnitTest.dll --platform:x64 | Tee-Object -FilePath '%BATCH_HOME%..\unit_test.result'"
+) else (
+  call "%VSTESTTOOL%" UnitTest.dll --platform:x64
+)
+ 
+cd %BATCH_HOME%UnitTest-CPP
+devenv UnitTest-CPP.sln /Upgrade
+devenv UnitTest-CPP.sln /Project UnitTest-CPP\UnitTest-CPP.vcxproj /Rebuild "Release|x64" 
+cd x64\Release
+ 
+if "%1"=="-p" (
+  powershell -Command "& '%VSTESTTOOL%' UnitTest-CPP.dll --platform:x64 | Tee-Object -FilePath '%BATCH_HOME%..\unit_test_cpp.result'"
+) else (
+  call "%VSTESTTOOL%" UnitTest-CPP.dll --platform:x64
+)
