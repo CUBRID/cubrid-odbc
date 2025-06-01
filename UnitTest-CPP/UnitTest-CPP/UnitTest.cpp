9dbdd4d46270cd9bdfd068b61f5bb0e7a3709e2d
Kisoo Han <kisoo-han@cubrid.com>
2025-06-02 08:51:33 +0900

    [APIS-1049] testcase for lowercase catalog query

--- UnitTest-CPP/UnitTest-CPP/UnitTest.cpp
+++ UnitTest-CPP/UnitTest-CPP/UnitTest.cpp
@@ -616,6 +616,45 @@ namespace UnitTestCPP
 			retcode = SQLFreeHandle(SQL_HANDLE_DBC, hDbc);
 			retcode = SQLFreeHandle(SQL_HANDLE_ENV, hEnv);
 		}
+
+		TEST_METHOD(APIS_1049_Lowercase_tablename_for_catalog_qry)
+		{
+			SQLHENV         hEnv;
+			SQLHDBC         hDbc;
+			SQLHSTMT        hstmt;
+			SQLINTEGER		retcode;
+			SQLWCHAR		*table_name = L"TEST_TBL1";
+			SQLWCHAR		*user = L"PUBLIC";
+			SQLWCHAR		query[512];
+
+			retcode = SQLAllocEnv(&hEnv);
+			retcode = SQLSetEnvAttr(hEnv, SQL_ATTR_ODBC_VERSION, (void *)SQL_OV_ODBC3, 0);
+			retcode = SQLAllocConnect(hEnv, &hDbc);
+			retcode = SQLDriverConnectW(hDbc, NULL, L"DRIVER=CUBRID Driver Unicode;server=test-db-server;port=33000;uid=public;pwd=;db_name=demodb;charset=utf-8;", SQL_NTS, NULL, 0, NULL, SQL_DRIVER_NOPROMPT);
+			Assert::AreNotEqual((int)retcode, SQL_ERROR);
+			retcode = SQLAllocHandle(SQL_HANDLE_STMT, hDbc, &hstmt);
+
+			wsprintf(query, L"DROP TABLE IF EXISTS %s", table_name);
+			retcode = SQLExecDirect(hstmt, query, SQL_NTS);
+
+			wsprintf(query, L"CREATE TABLE %s (col1 int)", table_name);
+			retcode = SQLExecDirect(hstmt, query, SQL_NTS);
+
+			retcode = SQLStatistics(hstmt,
+				NULL, 0,
+				user, SQL_NTS,
+				table_name, SQL_NTS,
+				SQL_INDEX_ALL,
+				SQL_QUICK);
+
+			Assert::AreNotEqual((int)retcode, SQL_ERROR);
+
+			retcode = SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
+			retcode = SQLDisconnect(hDbc);
+			retcode = SQLFreeHandle(SQL_HANDLE_DBC, hDbc);
+			retcode = SQLFreeHandle(SQL_HANDLE_ENV, hEnv);
+		}
+
 		TEST_METHOD(APIS_943_FOREIGNKEYSW)
 		{
 			SQLHENV         hEnv;
