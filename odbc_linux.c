38ff90ec9d00fb65226c08eeed4a98f0f0b51c04
Kisoo Han <kisoo-han@cubrid.com>
2025-10-03 17:05:50 +0900

    [APIS-1072] code style

--- odbc_linux.c
+++ odbc_linux.c
@@ -42,12 +42,12 @@ typedef enum
 PUBLIC INT_PTR CALLBACK ConfigDSNDlgProc (HWND hwndParent, UINT message, WPARAM wParam, LPARAM lParam);
 static char *find_key (char *string, char *key);
 static void dsn2connstr (CUBRIDDSNItem *dsn, char *connstr);
-extern char *odbcinst_system_file_path(char *b1);
-extern char *odbcinst_system_file_name(char *b2);
-extern char *odbcinst_user_file_path( char *buffer );
-extern char *odbcinst_user_file_name( char *buffer );
-extern BOOL _odbcinst_SystemINI(char *pszFileName, BOOL bVerify);
-extern BOOL _odbcinst_UserINI( char *pszFileName, BOOL bVerify);
+extern char *odbcinst_system_file_path (char *b1);
+extern char *odbcinst_system_file_name (char *b2);
+extern char *odbcinst_user_file_path (char *buffer);
+extern char *odbcinst_user_file_name (char *buffer);
+extern BOOL _odbcinst_SystemINI (char *pszFileName, BOOL bVerify);
+extern BOOL _odbcinst_UserINI (char *pszFileName, BOOL bVerify);
 static ODBCINI_DSN_LOOKUP_RESULT ini_fileopen (const char *dsn, HINI *hInip);
 
 #define FETCH_SIZE_DEFAULT	1
