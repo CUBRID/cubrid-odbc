de89153724fed3ed5de21a2c828d7abe64501931
Kisoo Han <kisoo-han@cubrid.com>
2025-10-17 18:43:18 +0900

    [APIS-1072] SQL_C_LONG: (long) for Windows (int) for Linux

--- odbc_type.c
+++ odbc_type.c
@@ -104,6 +104,12 @@ typedef struct tagC_DATA_TYPE_INFO
   long c_type_size;
 } C_DATA_TYPE_INFO;
 
+#if defined (_WINDOWS)
+typedef long sqlc_long_t;
+#else
+typedef int sqlc_long_t;
+#endif
+
 PRIVATE DATA_TYPE_INFO odbc_data_type_info_set[] =
 {
   {
@@ -161,8 +167,8 @@ PRIVATE DATA_TYPE_INFO odbc_data_type_info_set[] =
   },
 
   {
-    "INTEGER", SQL_INTEGER, SQL_C_LONG, 0, 10, sizeof (long), 11,
-    0, 10, NULL, sizeof (long), NULL, 11, NULL
+    "INTEGER", SQL_INTEGER, SQL_C_LONG, 0, 10, sizeof (sqlc_long_t), 11,
+    0, 10, NULL, sizeof (sqlc_long_t), NULL, 11, NULL
   },
 
   {
@@ -1551,8 +1557,8 @@ cci_value_to_odbc (void *c_value, short concise_type,
 	  break;
 	}
 
-      * (long *) c_value = cci_value->i;
-      length = sizeof (long);
+      * (sqlc_long_t *) c_value = cci_value->i;
+      length = sizeof (sqlc_long_t);
       break;
     case SQL_C_ULONG:
       if (cci_value->i > ULONG_MAX || cci_value->i < 0)
@@ -1562,8 +1568,8 @@ cci_value_to_odbc (void *c_value, short concise_type,
 	  break;
 	}
 
-      * (long *) c_value = cci_value->i;
-      length = sizeof (long);
+      * (sqlc_long_t *) c_value = cci_value->i;
+      length = sizeof (sqlc_long_t);
       break;
     case SQL_C_SBIGINT:
       if (cci_value->i > LLONG_MAX || cci_value->i < LLONG_MIN)
@@ -1820,7 +1826,7 @@ odbc_value_converter (VALUE_CONTAINER *target_value, VALUE_CONTAINER *src_value)
 	case SQL_C_SLONG:
 	case SQL_C_ULONG:
 	  target_value->value.l = atol (src_value->value.str);
-	  target_value->length = sizeof (long);
+	  target_value->length = sizeof (sqlc_long_t);
 	  break;
 	case SQL_C_SBIGINT:
 	case SQL_C_UBIGINT:
@@ -1872,8 +1878,8 @@ odbc_value_converter (VALUE_CONTAINER *target_value, VALUE_CONTAINER *src_value)
 	case SQL_C_LONG:
 	case SQL_C_SLONG:
 	case SQL_C_ULONG:
-	  target_value->value.l = (long) src_value->value.s;
-	  target_value->length = sizeof (long);
+	  target_value->value.l = (sqlc_long_t) src_value->value.s;
+	  target_value->length = sizeof (sqlc_long_t);
 	  break;
 	case SQL_C_SBIGINT:
 	case SQL_C_UBIGINT:
@@ -1928,7 +1934,7 @@ odbc_value_converter (VALUE_CONTAINER *target_value, VALUE_CONTAINER *src_value)
 	case SQL_C_SLONG:
 	case SQL_C_ULONG:
 	  target_value->value.l = src_value->value.l;
-	  target_value->length = sizeof (long);
+	  target_value->length = sizeof (sqlc_long_t);
 	  break;
 	case SQL_C_SBIGINT:
 	case SQL_C_UBIGINT:
@@ -1980,8 +1986,8 @@ odbc_value_converter (VALUE_CONTAINER *target_value, VALUE_CONTAINER *src_value)
 	case SQL_C_LONG:
 	case SQL_C_SLONG:
 	case SQL_C_ULONG:
-	  target_value->value.l = (long) src_value->value.bi;
-	  target_value->length = sizeof (long);
+	  target_value->value.l = (sqlc_long_t) src_value->value.bi;
+	  target_value->length = sizeof (sqlc_long_t);
 	  break;
 	case SQL_C_FLOAT:
 	  target_value->value.f = (float) src_value->value.bi;
@@ -2027,8 +2033,8 @@ odbc_value_converter (VALUE_CONTAINER *target_value, VALUE_CONTAINER *src_value)
 	case SQL_C_LONG:
 	case SQL_C_SLONG:
 	case SQL_C_ULONG:
-	  target_value->value.l = (long) src_value->value.f;
-	  target_value->length = sizeof (long);
+	  target_value->value.l = (sqlc_long_t) src_value->value.f;
+	  target_value->length = sizeof (sqlc_long_t);
 	  break;
 	case SQL_C_SBIGINT:
 	case SQL_C_UBIGINT:
@@ -2078,8 +2084,8 @@ odbc_value_converter (VALUE_CONTAINER *target_value, VALUE_CONTAINER *src_value)
 	case SQL_C_LONG:
 	case SQL_C_SLONG:
 	case SQL_C_ULONG:
-	  target_value->value.l = (long) src_value->value.d;
-	  target_value->length = sizeof (long);
+	  target_value->value.l = (sqlc_long_t) src_value->value.d;
+	  target_value->length = sizeof (sqlc_long_t);
 	  break;
 	case SQL_C_SBIGINT:
 	case SQL_C_UBIGINT:
@@ -2131,8 +2137,8 @@ odbc_value_converter (VALUE_CONTAINER *target_value, VALUE_CONTAINER *src_value)
 	case SQL_C_LONG:
 	case SQL_C_SLONG:
 	case SQL_C_ULONG:
-	  memcpy (&target_value->value.l, src_value->value.bin, sizeof (long));
-	  target_value->length = sizeof (long);
+	  memcpy (&target_value->value.l, src_value->value.bin, sizeof (sqlc_long_t));
+	  target_value->length = sizeof (sqlc_long_t);
 	  break;
 	case SQL_C_SBIGINT:
 	case SQL_C_UBIGINT:
